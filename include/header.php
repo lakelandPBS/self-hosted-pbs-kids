0fb94d50428cc01ab48457fe88fef73d106e29f6
webunraveling <jason@webunraveling.com>
2017-02-24 11:38:29 -0500

    Setup basic layout. Preparing for customizations.
    At this point we have the basic layout of the default PBS Kids site with
    no modifications.

--- include/header.php
+++ include/header.php
@@ -1,8 +1,9 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <title>Watch Lakeland PBS Kids</title>
-    
-        <link rel="stylesheet" type="text/css" href="library/css/base.css">
+
+        <?php // serving a cache busted CSS ?>
+        <link rel="stylesheet" type="text/css" href="library/css/base.css?<? echo filemtime('library/css/base.css'); ?>">
     </head>
     <body>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include_once('include/header.php'); ?>
 
-<iframe class="lptv-pbs-kids-video-module" src='http://pbskids.org/video/module/kawe' allowfullscreen ></iframe>
+<iframe class="pbs-kids-video-module" src='http://pbskids.org/video/module/kawe' allowfullscreen ></iframe>
 
 <?php include_once('include/footer.php'); ?>
