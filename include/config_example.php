1efb82627818637605b9fd4fbb10a6ff87eac0c4
webunraveling <jason@webunraveling.com>
2017-09-22 16:40:45 -0400

    Renaming config file
    The config file was not as obvious with the name variables.phpddd

---
+++ include/config_example.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * IMPORTANT: You must RENAME THIS FILE TO config.php
+ *
+ * All of the variables below are required for the page to work.
+ *
+ * Here is where all of your branding will go. Basically, 
+ * everything that makes the page specific to your station.
+ *
+ */
+
+// your call letters on record with PBS. This is needed for the
+// script that grabs the PBS Kids module. 
+$callLetters = 'kawe';
+
+// the page title that appears in the browser tab
+$pageTitle = 'Lakeland PBS Kids';
+
+// station logo (png is best)
+$stationLogo = 'images/logo.png';
+
+/* Currently not used. Th be used to create the menu
+ * once the menu is implemented
+// menu items ... keep this as short as possible
+// Add or remove menu items below. It works like this
+// 'Link Text' => 'http://site.org/your/URL/here'
+$stationMenuItems = array(
+    'Parents'       =>  '',
+    'Apps'          =>  '',
+    'TV Schedule'   =>  '',
+);
+
--- include/index.php
+++ include/index.php
@@ -1,5 +1,6 @@
 <?php
 
 // sssshhhhh
+die();
 
 ?>
